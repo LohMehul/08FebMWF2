2c11cc7e842e45e419ac0f300490ce4bb08b7757
jay-amin-tops <jay.amin.tops@gmail.com>
2023-03-20 15:06:24 +0530

    route class intro

--- ReactJS/topics/src/index.js
+++ ReactJS/topics/src/index.js
@@ -1,10 +1,10 @@
 
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-// import testing from 'testing';
-// import testing from './testing';
-// import Testing from './testing';
 import RootRouter from './router';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render("Testing");
 root.render(<RouterProvider router={RootRouter} />);
